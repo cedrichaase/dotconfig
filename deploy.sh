a4c43cb7d3f84d28fe0e3253188e793a941d7fcc
Cedric Haase <cedric@sineband.de>
2016-09-08 06:26:52 +0200

    Add plugins to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ DIR_DOTGEN=${DIR}/dotgen
 DIR_CONFIGS=${DIR}/configs
 DIR_OUTPUT=${DIR}/output
 
+DIR_PLUGINS=${DIR}/plugin
+DIR_PLUG_VIM=${DIR_PLUGINS}/vim
+DIR_PLUG_ZSH=${DIR_PLUGINS}/zsh
+
 DIR_TARGET=${HOME}
 
 function update()
@@ -27,13 +31,37 @@ function build()
 
 function install()
 {
+    # don't ignore dotfiles
     shopt -s dotglob
+
+    # install dotfiles
     cp -rs ${DIR_OUTPUT}/* ${DIR_TARGET}
+
+    # install zsh plugins
+    cp -rs ${DIR_PLUG_ZSH}/oh-my-zsh ${DIR_TARGET}/.oh-my-zsh
+
+    # install vim plugins
+    dir_vim_autoload=${DIR_TARGET}/.vim/autoload
+    [ ! -d  ${dir_vim_autoload} ] && mkdir -p ${dir_vim_autoload}
+
+    dir_vim_bundle=${DIR_TARGET}/.vim/bundle
+    [ ! -d  ${dir_vim_bundle} ] && mkdir -p ${dir_vim_bundle}
+
+    dir_vim_colors=${DIR_TARGET}/.vim/colors
+    [ ! -d  ${dir_vim_colors} ] && mkdir -p ${dir_vim_colors}
+
+    cp -rs ${DIR_PLUG_VIM}/vim-pathogen/autoload/* ${dir_vim_autoload}/
+    cp -rs ${DIR_PLUG_VIM}/rust.vim ${dir_vim_bundle}/
+    cp -rs ${DIR_PLUG_VIM}/sourcerer.vim ${dir_vim_bundle}/
+    ln -s ${DIR_PLUG_VIM}/sourcerer.vim/colors/sourcerer.vim ${dir_vim_colors}/
+
+    # ignore dotfiles again
     shopt -u dotglob
 }
 
 function all()
 {
+    update
     build
     install
 }
